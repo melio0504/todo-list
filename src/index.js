ce39f75110bf08d824a5f02c5fd60f73148b985c
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-12-19 22:03:31 +0800

    Add UI components to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.css';
 
-// Code here
+// Import UI
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import Container from './components/container';
+
+const root = document.querySelector('#root');
+
+// Load UI
+Header();
+Sidebar();
+Container;
