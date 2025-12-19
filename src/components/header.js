6e37158a6ea87f4000beb5dc7caa79aae5ce9dc9
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-12-20 01:43:45 +0800

    Add simple header

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,6 @@
 import Hamburger from '../../public/images/hamburger.png';
+import Logo from '../../public/images/logo.png';
+import Pfp from '../../public/images/pfp.png';
 
 const Header = () => {
   const header = document.createElement('header');
@@ -6,13 +8,25 @@ const Header = () => {
   const hamburgerBtn = document.createElement('img');
   hamburgerBtn.classList = 'header_hamburgerBtn';
   hamburgerBtn.src = Hamburger;
-  // hamburgerBtn.addEventListener('click', () => {
-
-  // })
+  hamburgerBtn.addEventListener('click', () => {
+    // Toggle sidebar
+  })
   header.appendChild(hamburgerBtn);
   
   const logo = document.createElement('img');
-  // logo.src = ;
+  logo.classList = 'header_logo';
+  logo.src = Logo;
+  header.appendChild(logo);
+
+  const logoName = document.createElement('h1');
+  logoName.classList = 'header_logoName';
+  logoName.textContent = 'Tasks';
+  header.appendChild(logoName);
+
+  const pfp = document.createElement('img');
+  pfp.classList = 'header_pfp';
+  pfp.src = Pfp;
+  header.appendChild(pfp);
 
   root.appendChild(header);
 };
