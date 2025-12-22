f54ae6cba02a0cedf08d67470311e5ac8580e71b
melio0504 <82772382+melio0504@users.noreply.github.com>
2025-12-22 23:10:33 +0800

    Remove UI components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,2 @@
 import './styles.css';
 
-// Import UI
-import Header from './components/header';
-import Sidebar from './components/sidebar';
-import Container from './components/container';
-
-const root = document.querySelector('#root');
-
-// Load UI
-Header();
-Sidebar();
-Container();
