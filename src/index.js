0094bf2b95decb137333f3767eb6a8936ebd9956
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-01-01 23:45:53 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import './styles.css';
-import toggleSidebar from './toggle-sidebar'
+import toggleSidebar from './toggle-sidebar';
+import DialogManager from './dialog-manager.js';
 
 // Toggle sidebar
-document.querySelector('.menu-toggle').addEventListener('click', toggleSidebar)
+document.querySelector('.menu-toggle').addEventListener('click', toggleSidebar);
+
+// Initialize dialog manager
+new DialogManager();
