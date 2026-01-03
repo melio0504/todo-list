e62d349a9e11d938579c8a147878451e39dec406
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-01-03 21:37:49 +0800

    Update webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,6 @@ module.exports = merge(common, {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    watchFiles: ["./index.html"],
-    open: true
+    watchFiles: ["./index.html"]
   },
 });
